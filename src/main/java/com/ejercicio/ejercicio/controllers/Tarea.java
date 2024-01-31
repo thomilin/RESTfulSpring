64a34ec16f5cc79e1c64ac596f32971c46e0887b
Thomas Contreras Guerrero <tcontrer@emeal.nttdata.com>
2024-01-31 12:57:18 -0300

    Levantando Proyecto

---
+++ src/main/java/com/ejercicio/ejercicio/controllers/Tarea.java
@@ -0,0 +1,15 @@
+package com.ejercicio.ejercicio.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class Tarea {
+
+    @RequestMapping(value = "prueba")
+    public List<String> prueba() {
+        return List.of("Frutas","Verduras","Carne");
+    }
+}
