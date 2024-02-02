f7613db5d03dab6510a2b32a66ce108df534fc54
Thomas <thomasignaciocg@gmail.com>
2024-02-02 13:23:28 -0300

    Listado tareas en la tabla

--- src/main/java/com/ejercicio/ejercicio/controllers/Test.java
+++ src/main/java/com/ejercicio/ejercicio/controllers/Test.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class Test {
 
@@ -15,6 +18,30 @@ public class Test {
         tarea.setNombre("Tarea1");
         tarea.setCompletado(true);
         return tarea;
+    }
+
+    @RequestMapping(value = "tareas")
+    public List<Tarea> getTareas() {
+        List<Tarea> tareas = new ArrayList<>();
+        Tarea tarea = new Tarea();
+        tarea.setId(1L);
+        tarea.setNombre("Tarea1");
+        tarea.setCompletado(true);
+
+        Tarea tarea2 = new Tarea();
+        tarea2.setId(2L);
+        tarea2.setNombre("Tarea2");
+        tarea2.setCompletado(false);
+
+        Tarea tarea3 = new Tarea();
+        tarea3.setId(3L);
+        tarea3.setNombre("Tarea3");
+        tarea3.setCompletado(true);
+
+        tareas.add(tarea);
+        tareas.add(tarea2);
+        tareas.add(tarea3);
+        return tareas;
     }
 
     /* @RequestMapping(value = "tarea12")
--- src/main/resources/static/js/tareas.js
+++ src/main/resources/static/js/tareas.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
 
 async function cargarTareas(){
 
-    const request = await fetch('tarea/1111', {
+    const request = await fetch('tareas', {
         method: 'GET',
         headers: {
             'Accept': 'application/json',
@@ -14,6 +14,12 @@ async function cargarTareas(){
     });
     const tareas = await request.json();
 
-    console.log(tareas);
+    let ListadoHtml = '';
+    for (let tarea of tareas){
+        let tareaHtml = '<tr class=\"alert\" role=\"alert\"><td><button type=\"button\">Edit</button></td><td class=\"d-flex align-items-center\"><div class=\"pl-3 email\"><span>'+tarea.nombre+'</span><span>Added: 01/03/2020</span></div></td><td>'+tarea.id+'</td><td class=\"status\"><span class=\"active\">'+tarea.completado+'</span></td><td><button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\"><span aria-hidden=\"true\"><i class=\"fa fa-close\"></i></span></button></td></tr>\n';
+        ListadoHtml += tareaHtml;
+    }
+
+    document.querySelector('#tareas tbody').outerHTML= ListadoHtml;
 
 }
